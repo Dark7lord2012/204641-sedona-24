29e23ead4f60cf8d0dc62b497639674615c5254b
Dark7lord <budda185@mail.ru>
2019-03-09 16:32:31 +0500

    попытка написать валидацию на JS, почему то не сбрасывается стандартное поведение кнопки Отправить при пустых полях

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 var buttonSearch = document.querySelector(".button-search");
 var searchHotelForm = document.querySelector(".search-hotels");
+var formSearchHotels = document.querySelector(".search-hotels");
+var dataArrival = formSearchHotels.querySelector("[name=date-arrival]");
+var dataADeparture = formSearchHotels.querySelector("[name=date-departure]");
+var numberMans = formSearchHotels.querySelector("[name=number-mans]");
+var numberChilds = formSearchHotels.querySelector("[name=number-childs]");
+var buttonSubmit = formSearchHotels.querySelector(".button-submit");
 
 buttonSearch.addEventListener("click", function(evt) {
   evt.preventDefault();
   searchHotelForm.classList.toggle("search-hotels-show");
 });
+
+buttonSubmit.addEventListener("click", function(evt) {
+  if(!dataArrival.value || !dataADeparture.value || !numberMans.value ||!numberChilds.value) {
+    evt.preventDefault;
+    console.log("Не все значения заданы или они пусты");
+  } else {
+    localStorage.setItem("dataArrival", dataArrival.value);
+  }
+});
+
+console.log(dataArrival.value);
+console.log(dataADeparture);
+console.log(numberMans);
+console.log(numberChilds);
+console.log(buttonSubmit);
